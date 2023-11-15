b9019c8c22628161cebc4394a0ed68a37b2d1d4f
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2023-11-15 14:12:34 +0900

    init commit

---
+++ hight-phase.py
@@ -0,0 +1,46 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import logging
+
+def calculate_phase_difference(R, C, f):
+    """
+    Calculate the phase difference of an RC high-pass filter.
+    
+    Parameters:
+    R (float): Resistance in ohms
+    C (float): Capacitance in farads
+    f (float): Frequency in hertz
+
+    Returns:
+    float: Phase difference in degrees
+    """
+    omega = 2 * math.pi * f
+    # Calculate phase difference
+    phase_diff = math.atan(1 / (omega * R * C))
+    # Convert to degrees
+    phase_diff = math.degrees(phase_diff)
+    return phase_diff
+
+if __name__ == "__main__":
+    R = 10000  # Resistance in ohms
+    C = 0.00000001 # Capacitance in farads
+    frequencies = [200, 1250, 1592, 5000, 20000]  # Frequencies in hertz
+    phase_differences = [calculate_phase_difference(R, C, f) for f in frequencies]
+    
+    # Log the data used for the plot
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Frequencies: %s", frequencies)
+    logging.info("Phase differences: %s", phase_differences)
+    
+    # Plot the frequency response
+    plt.figure()
+    plt.semilogx(frequencies, phase_differences)
+    plt.title('Phase difference of RC high-pass filter')
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Phase difference [degrees]')
+    plt.grid(True)
+    plt.show()
+
+
---
+++ low-frequency.py
@@ -0,0 +1,43 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import logging
+
+def calculate_theoretical_amplitude(R, C, f):
+    """
+    Calculate the theoretical amplitude of an RC low-pass filter.
+    
+    Parameters:
+    R (float): Resistance in ohms
+    C (float): Capacitance in farads
+    f (float): Frequency in hertz
+
+    Returns:
+    float: Theoretical amplitude
+    """
+    omega = 2 * math.pi * f
+    # Convert to decibels
+    omega = 20 * math.log10(1 / math.sqrt(1 + (omega * R * C) ** 2))
+    return omega
+
+if __name__ == "__main__":
+    R = 10000  # Resistance in ohms
+    C = 0.00000001 # Capacitance in farads
+    frequencies = [200, 1250, 1592, 5000, 20000]  # Frequencies in hertz
+    amplitudes = [calculate_theoretical_amplitude(R, C, f) for f in frequencies]
+    
+    # Log the data used for the plot
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Frequencies: %s", frequencies)
+    logging.info("Amplitudes: %s", amplitudes)
+    
+    # Plot the frequency response
+    plt.figure()
+    plt.semilogx(frequencies, amplitudes)
+    plt.title('Frequency response of RC low-pass filter')
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Amplitude [dB]')
+    plt.grid(True)
+    plt.show()
+
